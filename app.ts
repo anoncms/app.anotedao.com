9a51c0323ff5eedeb927cf78e4378f3f653c1221
Pragmatic <cryptopragmatic@protonmail.com>
2023-11-02 15:20:59 +0100

    Updates

--- app.ts
+++ app.ts
@@ -634,31 +634,37 @@ class Wallet {
     
                     var attachment = "";
                     if (recipient.startsWith('0x')) {
-                        attachment = libs.crypto.base58Encode(libs.crypto.stringToBytes(recipient));
-                        recipient = "3AQT89sRrWHqPSwrpfJAj3Yey7BCBTAy4jT";
-                        amount += 0.1;
+                        // attachment = libs.crypto.base58Encode(libs.crypto.stringToBytes(recipient));
+                        // recipient = "3AQT89sRrWHqPSwrpfJAj3Yey7BCBTAy4jT";
+                        // amount += 0.1;
+                        $("#sendError").html("Gateway is temporarily disabled. Try again later!");
+                        $("#sendError").fadeIn(function () {
+                            setTimeout(function () {
+                                $("#sendError").fadeOut();
+                            }, 2000);
+                        });
+                    } else {
+                        // recipient = "3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a";
+                        var transferOpts = {
+                            amount: Math.floor(amount * decimalPlaces),
+                            recipient: recipient,
+                            fee: fee,
+                            attachment: attachment
+                        }
+        
+                        if (currency != "") {
+                            transferOpts["assetId"] = currency;
+                        }
+        
+                        await this.signer.transfer(transferOpts).broadcast();
+                        $("#sendSuccess").fadeIn(function () {
+                            setTimeout(function () {
+                                $("#sendSuccess").fadeOut();
+                                $("#amount").val("");
+                                $("#addressRec").val("");
+                            }, 2000);
+                        });
                     }
-    
-                    // recipient = "3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a";
-                    var transferOpts = {
-                        amount: Math.floor(amount * decimalPlaces),
-                        recipient: recipient,
-                        fee: fee,
-                        attachment: attachment
-                    }
-    
-                    if (currency != "") {
-                        transferOpts["assetId"] = currency;
-                    }
-    
-                    await this.signer.transfer(transferOpts).broadcast();
-                    $("#sendSuccess").fadeIn(function () {
-                        setTimeout(function () {
-                            $("#sendSuccess").fadeOut();
-                            $("#amount").val("");
-                            $("#addressRec").val("");
-                        }, 2000);
-                    });
                 } catch (e: any) {
                     if (e.error == 112) {
                         console.log(e);
