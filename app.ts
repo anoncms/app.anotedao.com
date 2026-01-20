334496704e3cf2a59da20a6a3516e752d57708ab
Pragmatic <cryptopragmatic@protonmail.com>
2026-01-20 03:07:05 +0100

    Updates

--- app.ts
+++ app.ts
@@ -161,7 +161,7 @@ class Wallet {
     }
 
     initMiningSection() {
-        $.getJSON("https://nodes.aintchain.com/node/status", function (data) {
+        $.getJSON("https://node1.aintcoin.org/node/status", function (data) {
             var currentHeight = data.blockchainHeight;
             wallet.loadWalletHeight(currentHeight);
         });
@@ -171,9 +171,9 @@ class Wallet {
 
     // updateBlocks() {
     //     console.log("updateblocks");
-    //     $.getJSON("https://nodes.aintchain.com/node/status", function (data) {
+    //     $.getJSON("https://node1.aintcoin.org/node/status", function (data) {
     //         var currentHeight = data.blockchainHeight;
-    //         $.getJSON("https://nodes.aintchain.com/addresses/data/3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a?key=" + wallet.address, function (data) {
+    //         $.getJSON("https://node1.aintcoin.org/addresses/data/3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a?key=" + wallet.address, function (data) {
     //             if (data.length > 0) {
     //                 var miningData = data[0].value;
     //                 var mdSplit = miningData.split("__")
@@ -223,7 +223,7 @@ class Wallet {
 
     loadWalletHeight(currentHeight: number) {
         if (this.walletHeight == 0) {
-            $.getJSON("https://nodes.aintchain.com/addresses/data/3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a?key=" + wallet.address, function (data) {
+            $.getJSON("https://node1.aintcoin.org/addresses/data/3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a?key=" + wallet.address, function (data) {
                 if (data.length > 0) {
                     var miningData = data[0].value;
                     var mdSplit = miningData.split("__")
@@ -1458,7 +1458,7 @@ class Wallet {
     }
 
     async loadAintInfo() {
-        $.getJSON("https://nodes.aintchain.com/addresses/data/3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW", function (data) {
+        $.getJSON("https://node1.aintcoin.org/addresses/data/3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW", function (data) {
             data.forEach(function (entry) {
                 if (entry.key == "%s__price") {
                     var price = parseFloat(entry.value) / 100000000;
@@ -1476,7 +1476,7 @@ class Wallet {
             });
         });
 
-        $.getJSON("https://nodes.aintchain.com/assets/balance/3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW/9tZso8WvrB2YR5SA7RyCnsLcKjTZBGtQq49Js8cczeyb", function (data) {
+        $.getJSON("https://node1.aintcoin.org/assets/balance/3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW/9tZso8WvrB2YR5SA7RyCnsLcKjTZBGtQq49Js8cczeyb", function (data) {
             var total = parseFloat(data.balance) / 100000000;
             $("#aintTotal").val(total.toFixed(8));
         });
@@ -1622,11 +1622,11 @@ class Wallet {
 
     async populateTokens() {
         var tokenData;
-        await $.getJSON("https://nodes.aintchain.com/addresses/data/3ADqaKZpZrEEBSjZKqNemWrG3jzYUdUUYpi", function(data) {
+        await $.getJSON("https://node1.aintcoin.org/addresses/data/3ADqaKZpZrEEBSjZKqNemWrG3jzYUdUUYpi", function(data) {
             tokenData = data;
         });
 
-        $.getJSON("https://nodes.aintchain.com/assets/balance/" + this.address, function(data) {
+        $.getJSON("https://node1.aintcoin.org/assets/balance/" + this.address, function(data) {
             $.each(data.balances, function(i, b) {
                 var amount = b.balance / (10 ** b.issueTransaction.decimals);
                 var tokenListed = wallet.isTokenListed(tokenData, b.assetId);
@@ -1651,11 +1651,11 @@ class Wallet {
 
     private async initWaves(seed) {
         this.signer = new Signer({
-            NODE_URL: 'https://nodes.aintchain.com',
+            NODE_URL: 'https://node1.aintcoin.org',
         });
         this.provider = new ProviderSeed(seed);
         this.provider.connect({
-            NODE_URL: 'https://nodes.aintchain.com',
+            NODE_URL: 'https://node1.aintcoin.org',
             NETWORK_BYTE: 55,
         });
         this.signer.setProvider(this.provider);
@@ -1764,7 +1764,7 @@ class Wallet {
             });
         });
 
-        // $.getJSON("https://nodes.aintchain.com/addresses/data/3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a", function(data){
+        // $.getJSON("https://node1.aintcoin.org/addresses/data/3ANzidsKXn9a1s9FEbWA19hnMgV9zZ2RB9a", function(data){
         //     data.forEach(function (entry) {
         //         try {
         //             var height = entry.value.split("__")[1];
@@ -1780,13 +1780,13 @@ class Wallet {
     }
 
     async getAdNumber() {
-        $.getJSON("https://nodes.aintchain.com/addresses/data/3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW/%25s__adnum", function (data) {
+        $.getJSON("https://node1.aintcoin.org/addresses/data/3ANmnLHt8mR9c36mdfQVpBtxUs8z1mMAHQW/%25s__adnum", function (data) {
             $("#buttonCode").attr("href", "https://t.me/AnoteAds/" + data.value);
         });
     }
 
     private async checkAlias() {
-        $.getJSON("https://nodes.aintchain.com/alias/by-address/" + this.address, function (data) {
+        $.getJSON("https://node1.aintcoin.org/alias/by-address/" + this.address, function (data) {
             if (data.length > 0) {
                 var alias = String(data[0]).replace("alias:7:", "");
                 $("#alias").val(alias);
@@ -1799,7 +1799,7 @@ class Wallet {
 
     private async populateStaking() {
         var stakingKey = "%25s__" + wallet.address;
-        $.getJSON("https://nodes.aintchain.com/addresses/data/3A9y1Zy78DDApbQWXKxonXxci6DvnJnnNZD?key=" + stakingKey, function (data) {
+        $.getJSON("https://node1.aintcoin.org/addresses/data/3A9y1Zy78DDApbQWXKxonXxci6DvnJnnNZD?key=" + stakingKey, function (data) {
             var amountStaked = 0.0;
             if (data.length > 0) {
                 amountStaked = parseFloat(data[0].value.split("__")[1]) / 100000000;
@@ -1823,7 +1823,7 @@ class Wallet {
                 // console.log(wallet.aintPrice);
                 // console.log(amountFromAint);
 
-                $.getJSON("https://nodes.aintchain.com/addresses/data/3AR11vcAeEfWFMTKbcxTo79LcbH7uSmhftZ?key=" + stakingKey, function (data1) {
+                $.getJSON("https://node1.aintcoin.org/addresses/data/3AR11vcAeEfWFMTKbcxTo79LcbH7uSmhftZ?key=" + stakingKey, function (data1) {
                     var amountStakedAint = 0.0;
                     if (data1.length > 0) {
                         amountStakedAint = parseFloat(data1[0].value.split("__")[1]) / 100000000;
@@ -1840,7 +1840,7 @@ class Wallet {
             });
         });
 
-        $.getJSON("https://nodes.aintchain.com/addresses/data/3AR11vcAeEfWFMTKbcxTo79LcbH7uSmhftZ?key=" + stakingKey, function (data) {
+        $.getJSON("https://node1.aintcoin.org/addresses/data/3AR11vcAeEfWFMTKbcxTo79LcbH7uSmhftZ?key=" + stakingKey, function (data) {
             var amountStaked = 0.0;
             if (data.length > 0) {
                 amountStaked = parseFloat(data[0].value.split("__")[1]) / 100000000;
@@ -1848,7 +1848,7 @@ class Wallet {
             $("#stakedAmountAnote").val(amountStaked.toFixed(8));
         });
 
-        $.getJSON("https://nodes.aintchain.com/addresses/data/3AVTze8bR1SqqMKv3uLedrnqCuWpdU7GZwX", function (data) {
+        $.getJSON("https://node1.aintcoin.org/addresses/data/3AVTze8bR1SqqMKv3uLedrnqCuWpdU7GZwX", function (data) {
             var showNodeStake = false;
             var buttonNum = 0;
             $("#dropdownMenu2").html("");
@@ -1865,7 +1865,7 @@ class Wallet {
                         $("#dropdownMenuButton2").html(this.innerHTML);
                         wallet.stakeType = this.innerHTML.replace("Node: ", "");
                         var stakingKey = "%25s__" + wallet.stakeType;
-                        $.getJSON("https://nodes.aintchain.com/addresses/data/3A9y1Zy78DDApbQWXKxonXxci6DvnJnnNZD?key=" + stakingKey, function (data) {
+                        $.getJSON("https://node1.aintcoin.org/addresses/data/3A9y1Zy78DDApbQWXKxonXxci6DvnJnnNZD?key=" + stakingKey, function (data) {
                             var amountStaked = 0.0;
                             if (data.length > 0) {
                                 amountStaked = parseFloat(data[0].value.split("__")[1]) / 100000000;
@@ -1887,7 +1887,7 @@ class Wallet {
 
     private async checkReferral() {
         if (this.referral && this.referral.length > 0 && !this.referral.startsWith("3A") && this.referral != undefined) {
-            $.getJSON("https://nodes.aintchain.com/alias/by-alias/" + this.referral, function (data) {
+            $.getJSON("https://node1.aintcoin.org/alias/by-alias/" + this.referral, function (data) {
                 if (data.address) {
                     wallet.referral = data.address;
                     localStorage.removeItem("referral");
@@ -1949,11 +1949,11 @@ class Wallet {
             try {
                 var seed = libs.crypto.decryptSeed(this.seed, String(password));
                 var signer = new Signer({
-                    NODE_URL: 'https://nodes.aintchain.com',
+                    NODE_URL: 'https://node1.aintcoin.org',
                 });
                 var provider = new ProviderSeed(seed);
                 provider.connect({
-                    NODE_URL: 'https://nodes.aintchain.com',
+                    NODE_URL: 'https://node1.aintcoin.org',
                     NETWORK_BYTE: 55,
                 });
                 signer.setProvider(provider);
@@ -2305,7 +2305,7 @@ $("#aintButton").on("click", function () {
 //     wallet.stakeType = "mobile";
 //     $("#dropdownMenuButton2").html("Mobile Mining");
 //     var stakingKey = "%25s__" + wallet.getAddress();
-//     $.getJSON("https://nodes.aintchain.com/addresses/data/3A9y1Zy78DDApbQWXKxonXxci6DvnJnnNZD?key=" + stakingKey, function (data) {
+//     $.getJSON("https://node1.aintcoin.org/addresses/data/3A9y1Zy78DDApbQWXKxonXxci6DvnJnnNZD?key=" + stakingKey, function (data) {
 //         var amountStaked = 0.0;
 //         if (data.length > 0) {
 //             amountStaked = parseFloat(data[0].value.split("__")[1]) / 100000000;
